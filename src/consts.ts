b869fbf6e18bc12b0be82e055367e3810b76e795
wyp <wyp@shmtu.edu.cn>
2025-08-17 21:11:57 +0800

    update one article

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "零一格物，01World";
+export const SITE_DESCRIPTION = "在零一格物，数字是语言，AI是工具，我们认知世界，也重构世界。";
