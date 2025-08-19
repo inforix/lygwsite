23329c7639ecc58918f43c30a9fb9454b8ea8104
wyp <wyp@shmtu.edu.cn>
2025-08-19 11:21:06 +0800

    udpate css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            fontSize: '1em',
+            fontFamily: '"PingFang SC", "Microsoft YaHei", sans-serif;',
+            a:{
+              color: '#00c',
+              textDecoration: 'none',
+            },
+            h1:{
+              color: '#039',
+            },
+            h2: {
+              color: '#039',
+            },
+            h3: {
+              color: '#039',
+            },
+            h4: {
+              color: '#039',
+            }
+          },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
